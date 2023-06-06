f07c966a41c4cca1524e39d90100c41c81fe70a2
Mahmoud <mahmoud.talass@gmail.com>
2023-06-06 12:33:02 -0400

    Display a confirmation wether the password are eql
    Prevent from from submitting if passwords are not equal.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.querySelector("form");
 const inputs = form.querySelectorAll("input");
+const password = document.querySelector("#password");
+const confirmPwrd = document.querySelector("#confirm");
+const errorMsg = document.querySelector("#confirmation-msg");
 
 inputs.forEach((input) => {
    input.addEventListener("input", () => {
@@ -8,4 +12,23 @@ inputs.forEach((input) => {
          input.classList.add("invalid-input");
       }
    });
+});
+
+let areEqual;
+
+confirmPwrd.addEventListener("input", () => {
+   areEqual = confirmPwrd.value === password.value;
+   if (!areEqual) {
+      errorMsg.textContent = "Passwords do not match";
+      errorMsg.className = "no-match";
+   } else {
+      errorMsg.textContent = "Passwords match!";
+      errorMsg.className = "match";
+   }
+});
+
+form.addEventListener("submit", (e) => {
+   if (!areEqual) {
+      e.preventDefault();
+   }
 });
